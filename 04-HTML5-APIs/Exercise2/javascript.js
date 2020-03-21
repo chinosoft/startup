d46b5aa962aeb331acbacbb5edbe6a6b622258a3
Marcelo Benitez <chinosoft@gmail.com>
2020-03-21 16:25:22 -0300

    Exercise2 Drag and Drop

---
+++ 04-HTML5-APIs/Exercise2/javascript.js
@@ -0,0 +1,16 @@
+function dropHandler(ev) {
+  ev.preventDefault();
+  if (ev.dataTransfer.items) {
+    for (var i = 0; i < ev.dataTransfer.items.length; i++) {
+      if (ev.dataTransfer.items[i].kind === 'file') {
+        var file = ev.dataTransfer.items[i].getAsFile();
+        console.log('... file[' + i + '].name = ' + file.name);
+      }
+    }
+  } else {
+    for (var i = 0; i < ev.dataTransfer.files.length; i++) {
+      console.log('... file[' + i + '].name = ' + ev.dataTransfer.files[i].name);
+    }
+  } 
+  removeDragData(ev)
+}
